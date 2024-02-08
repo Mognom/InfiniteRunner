4ad2e8b041a52015e041269fbe143eb502e0a2fc
Mognom <alex.mognom@gmail.com>
2024-02-08 19:26:38 +0100

    Stop ragdoll from collecting coins
    Also, improve a bit the coin pickup sound so its less annoying at high difficulties

--- Assets/Scripts/Collectibles/CollectibleBehaviour.cs
+++ Assets/Scripts/Collectibles/CollectibleBehaviour.cs
@@ -5,6 +5,7 @@ public class CollectibleBehaviour : MonoBehaviour {
     [SerializeField] private GameObject collectibleVisual;
     [SerializeField] private int scoreValue;
     [SerializeField] private IntEventChannel scoreCollectedChannel;
+    [SerializeField] private string playerTag;
 
     private AudioSource audioSource;
 
@@ -17,7 +18,7 @@ public class CollectibleBehaviour : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider player) {
-        if (player != null) {
+        if (player != null && player.gameObject.CompareTag(playerTag)) {
             scoreCollectedChannel.PostEvent(scoreValue);
             audioSource.Play();
             collectibleVisual.SetActive(false);
